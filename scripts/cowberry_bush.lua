97a1ef4553cc2a1e6223feb16f5ee01f8732d667
EsPaKira <kirillplay1361@gmail.com>
2026-04-29 22:05:45 +0400

    new biome, items and blocks

---
+++ scripts/cowberry_bush.lua
@@ -0,0 +1,13 @@
+function on_interact(x, y, z, playerid)
+    local blackberry_slot = inventory.find_by_item(player.get_inventory(playerid), item.index("newgen:cowberry"))
+    local free_slot = inventory.find_by_item(player.get_inventory(playerid), 0)
+    if blackberry_slot ~= nil or free_slot ~= nil then
+        block.set_variant(x, y, z, 1)
+        inventory.add(player.get_inventory(playerid), item.index("newgen:cowberry"), random.random(2,5))
+        return
+    end
+end
+
+function on_random_update(x, y, z)
+    block.set_variant(x, y, z, 0)
+end
